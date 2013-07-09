ed95b11f2ab65e713d862c45936f4873edeb7a54
trollfot <trollfot@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2013-07-09 13:14:29 +0000

    Changed template to simplify

--- uvckickstart/templates/uvclight/src/+ppackage+/src/+ppackage+/utils.py
+++ uvckickstart/templates/uvclight/src/+ppackage+/src/+ppackage+/utils.py
@@ -16,31 +16,10 @@ from cromlech.dawnlight import view_locator, query_view
 from zope.component import getGlobalSiteManager
 
 
-def configuration(global_conf, zcml_file, langs):
-    """A factory for the configuration middleware.
-    It's usually used to initialize some parameters,
-    register components and possibly wrap the requested
-    apps.
-    """
-
-    load_zcml(zcml_file)
-
-    allowed = langs.strip().replace(',', ' ').split()
-    register_allowed_languages(allowed)
-
-    def app_wrapper(app):
-        """The effective middleware. Here, we do not make use
-        of the wrapping capabilities, but it can be used to
-        add other explicit middlewares.
-        """
-        return app
-
-    return app_wrapper
-
 view_lookup = ViewLookup(view_locator(query_view))
 
 
-class Site(Location):
+class Root(Location):
     implements(IPublicationRoot)
 
     title = u"Example Site"
@@ -53,23 +32,31 @@ class Site(Location):
         return getGlobalSiteManager()
 
 
-def app(global_conf, name, zcml_file=None, **kwargs):
-    """A factory used to bootstrap the TrajectApplication.
-    As the TrajectApplication will use SQL, we use this
-    'once and for all' kind of factory to configure the
-    SQL connection and inject the demo datas.
-    """
-    if zcml_file:
-        load_zcml(zcml_file)
+class Site(object):
 
-    def publisher(environ, start_response):
-        request = Request(environ)
-        site = Site(name)
-        setSite(site)
-        notify(PublicationBeginsEvent(site, request))
-        publisher = DawnlightPublisher(view_lookup=view_lookup)
-        response = publisher.publish(request, site, handle_errors=True)
-        notify(PublicationEndsEvent(request, response))
+    def __init__(self, name):
+        self.name = name
+    
+    def __enter__(self):
+        root = Root(self.name)
+        setSite(root)
+        return root
+        
+    def __exit__(self, exc_type, exc_value, traceback):
         setSite()
+        
+
+class Application(object):
+
+    def __init__(self, global_conf, name, zcml_file=None, langs='en'):
+        load_zcml(zcml_file)
+        allowed = langs.strip().replace(',', ' ').split()
+        register_allowed_languages(allowed)
+        self.name = name
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        with Site(self.name) as site:
+            publisher = DawnlightPublisher(view_lookup=view_lookup)
+            response = publisher.publish(request, site, handle_errors=True)
         return response(environ, start_response)
-    return publisher

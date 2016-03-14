aa10407c112f397914d5ddf5af096e3cd676e92c
Christian Klinger <ck@novareto.de>
2016-03-14 16:43:55 +0100

    chagnes

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+
 sudo apt-get install python-dev python-virtualenv python-setuptools subversion git libxml2-dev libssl-dev libxslt1-dev git libreadline-dev wv poppler-utils libjpeg62-dev build-essential vim
 virtualenv playground 
 playground/bin/easy_install -f http://dev.bg-kooperation.de/pypi/simple uvckickstart
 
-PATH=$PATH:/${PWD}/playground/bin/
-
-echo "Bitte exportiert das VERZEICHNIS IN EUER ENVIRONMENT"
-echo "export PATH '$PATH'"
+PATH=$PATH:${PWD}/playground/bin/
+echo "export PATH='$PATH'" >> $HOME/.profile
+source $HOME/.profile

3ed7d67826b8c0d75635d587227a4caf2af8c712
Christian Klinger <ck@novareto.de>
2016-03-14 16:01:15 +0100

    fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-sudo apt-get install python-dev python-virtualenv python-setuptools subversion git libxml2-dev libssl-dev libxslt1-dev git libreadline-dev wv poppler-utils libjpeg62-dev build-essential
-virtualenv playground vim
+sudo apt-get install python-dev python-virtualenv python-setuptools subversion git libxml2-dev libssl-dev libxslt1-dev git libreadline-dev wv poppler-utils libjpeg62-dev build-essential vim
+virtualenv playground 
 playground/bin/easy_install -f http://dev.bg-kooperation.de/pypi/simple uvckickstart
 
 pp=${PWD}/playground/bin

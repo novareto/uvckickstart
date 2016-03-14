71c59d4c848f5d0a000a1c8af9f07998dc1a8473
Christian Klinger <ck@novareto.de>
2016-03-14 15:27:47 +0100

    add environment

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 sudo apt-get install python-dev python-virtualenv python-setuptools subversion git libxml2-dev libssl-dev libxslt1-dev git libreadline-dev wv poppler-utils libjpeg62-dev build-essential
 virtualenv playground
 playground/bin/easy_install -f http://dev.bg-kooperation.de/pypi/simple uvckickstart
+
+pp=${PWD}/playground/bin
+echo $pp
+PATH=$pp:$PATH
+echo $PATH
+export PATH

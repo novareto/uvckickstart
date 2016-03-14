9445a4d98ba3c00aa528878bcf180dacaac238bc
Christian Klinger <ck@novareto.de>
2016-03-14 14:53:03 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo apt-get install python-dev python-virtualenv python-setuptools subversion git libxml2-dev libssl-dev libxslt1-dev git libreadline-dev wv poppler-utils libjpeg62-dev build-essential
+virtualenv playground
+playground/bin/easy_install -f http://dev.bg-kooperation.de/pypi/simple uvckickstart
